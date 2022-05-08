172aee1a3715751644b4fa6acbb371d4f2bdf1d8
Bartjan Henkemans <bartjan@henkemans.be>
2022-05-08 22:09:29 +0200

    Dockerization complete.

---
+++ stop-win.ps1
@@ -0,0 +1,17 @@
+Clear-Host
+
+Write-Output "What operating system are you running?"
+Write-Output "1) Windows"
+Write-Output "2) Linux"
+$os = read-host 'Boot option: '
+
+if ($os -eq 1) {
+    Write-Output "Shutting down LAMA."
+    docker compose -f Docker/docker-compose.yml down 
+    Write-Output "Done. Goodbye!"
+    exit
+} else {
+    Write-Output "You gave an invalid option for the OS (either not 1/2 or Linux)."
+    Write-Output "Try again by reopening the file. Goodbye for now!"
+    exit
+}
