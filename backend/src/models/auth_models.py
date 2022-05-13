1569dbdc17dc08502fe4a5a4dcfffabf05b15838
eduardo-devintage <63951752+eduardo-devintage@users.noreply.github.com>
2022-05-13 11:45:50 +0200

    Update user model and remove old route
    This will almost definitely break the front-end too, but that's okay since it was a placeholder anyway

--- backend/src/models/auth_models.py
+++ backend/src/models/auth_models.py
@@ -1,10 +1,13 @@
-from src import db, ma
+from src import db, ma # need this in every model
 
 class User(db.Model): # Obviously needs to be fleshed out
     __tablename__ = "Users"
     id = db.Column(db.Integer, primary_key=True) # auto_increment=True is default for integer primary key
-    username = db.Column(db.String(32))
+    username = db.Column(db.String(32), unique=True)
     password = db.Column(db.String(64))
+    email = db.Column(db.EmailType, unique=True)
+    approved = db.Column(db.Integer) # Approved by super-admin -1 => denied, 0 => pending, 1 => approved
+    description = db.Column(db.String) # Personal description
 
 class UserSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
