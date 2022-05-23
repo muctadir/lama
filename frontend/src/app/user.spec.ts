25c670f89811c8c70e7b124f714b892149023144
JarlJansen123 <jarljansen123@gmail.com>
2022-05-23 13:29:05 +0200

    Added test cases + improved comments
    Added test cases + comments to the input-check service. Also resolved compile error in user.spec.ts, and added actual test cases there.

--- frontend/src/app/user.spec.ts
+++ frontend/src/app/user.spec.ts
@@ -1,7 +1,31 @@
 import { User } from './user';
 
 describe('User', () => {
-  it('should create an instance', () => {
-    expect(new User()).toBeTruthy();
-  });
+    let user: User;
+
+    // Initializes the user
+    beforeEach(() => {
+        user = new User(1, "testname", "test@gmail.com", "This is a test user");
+    });
+
+    // Checks whether the id is returned correctly.
+    it('Should return the ID of the user', () => {
+        expect(user.getId()).toBe(1);
+    });
+
+    // Checks whether the username is returned correctly.
+    it('Should return the username of the user', () => {
+        expect(user.getUsername()).toBe("testname");
+    });
+
+    // Checks whether the email is returned correctly.
+    it('Should return the Email of the user', () => {
+        expect(user.getEmail()).toBe("test@gmail.com");
+    });
+
+    // Checks whether the email is returned correctly.
+    it('Should return the Email of the user', () => {
+        expect(user.getDescription()).toBe("This is a test user");
+    });  
+    
 });
