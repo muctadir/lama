f6871b36211969d28fac83b5fc5270d80f402519
Victoria <victoria.bogachenkova@gmail.com>
2022-05-24 08:16:54 +0200

    Make the plus button open a module with username

--- frontend/src/app/project-creation/project-creation.component.ts
+++ frontend/src/app/project-creation/project-creation.component.ts
@@ -1,5 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
+@Component({
+  selector: 'app-project-creation',
+  styleUrls: ['./project-creation.component.scss'],
+  template: `
+    <div class="modal-header">
+      <h4 class="modal-title">Choose the users to add to the project</h4>
+      <button type="button" class="btn-close" aria-label="Close" (click)="activeModal.dismiss('Cross click')"></button>
+    </div>
+    <div class="modal-body row" *ngFor="let user of users" style="padding:2px 16px; max-height: 25px;">
+        <div class="col" style="padding:1px; list-style: none; max-height: 25px;">
+          <li> {{ user.userName }}</li>
+
+        </div>
+        <!-- Col for adding users button -->
+        <div class="col-2">
+            <!-- Button for adding new members -->
+            <button class="btn" type="button" id="addMembersButton" (click)="addUser()">
+                <svg class="bi bi-plus members" viewBox="0 0 16 16">
+                    <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                </svg>
+            </button>
+        </div>
+        <br>
+    </div>
+    
+    <div class="modal-footer">
+      <button type="button" class="btn btn-outline-dark" (click)="activeModal.close('Close click')">Close</button>
+    </div>
+  `
+})
+
+export class AddUsersModalContent {
+  @Input() users: User[] = [];
+
+  constructor(public activeModal: NgbActiveModal) {}
+
+  // Function for adding a new user input
+  addUser(){
+    this.users.push();
+  }
+}
 
 // Project object
 interface Project {
@@ -18,7 +60,6 @@ interface User {
 })
 
 export class ProjectCreationComponent implements OnInit {
-
 
   // Functions for adding values
   addValuesProject(name:string, desc:string):Project {
@@ -44,14 +85,14 @@ export class ProjectCreationComponent implements OnInit {
   Chinno3 = this.addValuesUser("Chinno3");
   Chinno4 = this.addValuesUser("Chinno4");
   Chinno5 = this.addValuesUser("Chinno5");
+  Chinno6 = this.addValuesUser("Chinno6");
+  Chinno7 = this.addValuesUser("Chinno7");
 
-  // Array of projects
-  members: User[] = [this.Veerle, this.Vic, this.Bartjan, this.Jarl, this.Chinno, this.Chinno2, this.Chinno3, this.Chinno4, this.Chinno5];
+  // Array of memebers
+  members: User[] = [this.Veerle, this.Vic, this.Bartjan, this.Jarl, this.Chinno, this.Chinno2, this.Chinno3, this.Chinno4, this.Chinno5, this.Chinno6, this.Chinno7];
   
   // Label types
   labelTypes: string[] = ["doing"];
-
-  constructor() { }
 
   ngOnInit(): void { }
 
@@ -110,4 +151,13 @@ export class ProjectCreationComponent implements OnInit {
   addLabelType(){
     this.labelTypes.push("");
   }
-}
+
+  constructor(private modalService: NgbModal) {}
+
+  open() {
+    const modalRef = this.modalService.open(AddUsersModalContent);
+    modalRef.componentInstance.users = this.members;
+  }
+  
+ }
+
