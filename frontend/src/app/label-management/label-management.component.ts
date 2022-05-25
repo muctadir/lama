b8c5a3e7d553f02f3cfbba3cf071eb778607b27e
Victoria <victoria.bogachenkova@gmail.com>
2022-05-25 07:53:07 +0200

    Started on the modal

--- frontend/src/app/label-management/label-management.component.ts
+++ frontend/src/app/label-management/label-management.component.ts
@@ -1,4 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+// Victoria Bogachenkova
+
+// // Project object
+// interface Project {
+//   projectName: string,
+//   projectDescription: string;
+// }
+
+// // User object
+// interface User {
+//   userName: string;
+// }
+
+// Template for the modal
+@Component({
+  selector: 'app-label-management',
+  styleUrls: ['./label-management.component.scss'],
+  template: `
+    <div class="modal-header">
+      <h4 class="modal-title">Choose the users to add to the project</h4>
+      <button type="button" class="btn-close" aria-label="Close" (click)="activeModal.dismiss('Cross click')"></button>
+    </div>
+    <div class="modal-body row" *ngFor="let user of users" style="padding:2px 16px; max-height: 25px;">
+        <div class="col" style="padding:15 1 1 1px; list-style: none; max-height: 25px;">
+          <li> {{ user.userName }}</li>
+        </div>
+        <!-- Col for adding users button -->
+        <div class="col-2">
+            <!-- Button for adding new project_members -->
+            <button class="btn" type="button" id="addMembersButton" (click)="addUser(user.userName)">
+              <!-- Plus icon -->
+              <i class="bi bi-plus labelType"></i>
+            </button>
+        </div>
+        <br>
+    </div>
+    
+    <div class="modal-footer">
+      <button type="button" class="btn btn-outline-dark" (click)="activeModal.close('Close click')">Close</button>
+      </div>
+  `
+})
+
+// Content of the modal
+export class AddUsersModalContent {
+  @Input() users: any;
+  @Output() newItemEvent = new EventEmitter<any>();
+
+  constructor(public activeModal: NgbActiveModal) {}
+
+  addUser(name:string) {
+    this.newItemEvent.emit(name);
+  }
+}
 
 @Component({
   selector: 'app-label-management',
