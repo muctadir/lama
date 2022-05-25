82681d2b96906c1d0848e06c325042afbd5c18e7
Victoria <victoria.bogachenkova@gmail.com>
2022-05-25 08:01:14 +0200

    Create the modals for both merge and create labels buttons

--- frontend/src/app/label-management/label-management.component.ts
+++ frontend/src/app/label-management/label-management.component.ts
@@ -2,39 +2,17 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 // Victoria Bogachenkova
 
-// // Project object
-// interface Project {
-//   projectName: string,
-//   projectDescription: string;
-// }
-
-// // User object
-// interface User {
-//   userName: string;
-// }
-
 // Template for the modal
 @Component({
   selector: 'app-label-management',
   styleUrls: ['./label-management.component.scss'],
   template: `
     <div class="modal-header">
-      <h4 class="modal-title">Choose the users to add to the project</h4>
+      <h4 class="modal-title">Merge Labels</h4>
       <button type="button" class="btn-close" aria-label="Close" (click)="activeModal.dismiss('Cross click')"></button>
     </div>
     <div class="modal-body row" *ngFor="let user of users" style="padding:2px 16px; max-height: 25px;">
-        <div class="col" style="padding:15 1 1 1px; list-style: none; max-height: 25px;">
-          <li> {{ user.userName }}</li>
-        </div>
-        <!-- Col for adding users button -->
-        <div class="col-2">
-            <!-- Button for adding new project_members -->
-            <button class="btn" type="button" id="addMembersButton" (click)="addUser(user.userName)">
-              <!-- Plus icon -->
-              <i class="bi bi-plus labelType"></i>
-            </button>
-        </div>
-        <br>
+        
     </div>
     
     <div class="modal-footer">
@@ -44,15 +22,36 @@ import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 })
 
 // Content of the modal
-export class AddUsersModalContent {
-  @Input() users: any;
-  @Output() newItemEvent = new EventEmitter<any>();
+export class MergeLabelsModalContent {
 
   constructor(public activeModal: NgbActiveModal) {}
 
-  addUser(name:string) {
-    this.newItemEvent.emit(name);
-  }
+}
+
+// Template for the modal
+@Component({
+  selector: 'app-label-management',
+  styleUrls: ['./label-management.component.scss'],
+  template: `
+    <div class="modal-header">
+      <h4 class="modal-title">Create Label</h4>
+      <button type="button" class="btn-close" aria-label="Close" (click)="activeModal.dismiss('Cross click')"></button>
+    </div>
+    <div class="modal-body row" *ngFor="let user of users" style="padding:2px 16px; max-height: 25px;">
+        
+    </div>
+    
+    <div class="modal-footer">
+      <button type="button" class="btn btn-outline-dark" (click)="activeModal.close('Close click')">Close</button>
+      </div>
+  `
+})
+
+// Content of the modal
+export class CreateLabelsModalContent {
+
+  constructor(public activeModal: NgbActiveModal) {}
+
 }
 
 @Component({
@@ -62,7 +61,29 @@ export class AddUsersModalContent {
 })
 export class LabelManagementComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalService: NgbModal) {}
+
+  // Open the modal and populate it with users
+  openMerge() {
+    const modalRef = this.modalService.open(MergeLabelsModalContent);
+    // modalRef.componentInstance.users = this.all_members;
+    // // Push the username into the members list 
+    // modalRef.componentInstance.newItemEvent.subscribe(($e: any) => {
+    //   var username = {userName: $e};
+    //   this.project_members.push(username);
+    // })
+  }
+
+  // Open the modal and populate it with users
+  openCreate() {
+    const modalRef = this.modalService.open(CreateLabelsModalContent);
+    // modalRef.componentInstance.users = this.all_members;
+    // // Push the username into the members list 
+    // modalRef.componentInstance.newItemEvent.subscribe(($e: any) => {
+    //   var username = {userName: $e};
+    //   this.project_members.push(username);
+    // })
+  }
 
   ngOnInit(): void {
   }
