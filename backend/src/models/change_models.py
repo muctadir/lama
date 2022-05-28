cd5a6cd754206fe0816939f97ab02730fe4a45c4
eduardo-devintage <63951752+eduardo-devintage@users.noreply.github.com>
2022-05-28 19:04:07 +0200

    Labelling relationship, Change models in own file
    Labelling relationship is still just a framework

---
+++ backend/src/models/change_models.py
@@ -0,0 +1,93 @@
+from src.models import db, ma
+from sqlalchemy.orm import declarative_mixin, declared_attr
+from src.app_util import AppUtil
+
+# TODO: Enforce Change to be abstract
+
+def create_change_table(cls):
+    """
+    For a ChangingItem with name 'Item' and table name 'item', this function is a class factory.
+    It returns a concrete Change class with name 'ItemChange' and table name 'item_change'.
+    This allows us to automatically create all the changelog tables for the items,
+    since all classes inheriting db.Model create a table.
+    """
+
+    # type function can create classes
+    return type(cls.__name__ + 'Change',  # class name
+            (Change, db.Model),  # base classes
+            {'__tablename__': cls.__tablename__ + '_change'})  # added attributes/methods
+
+@declarative_mixin
+class Change():
+    """
+    The Changelog base class. We call this class change because an instance represents
+    a single change, not a log of changes.
+    A table is created by the declaration of a class. All changelogs are the same, but
+    require different tables. Thus we dynamically create subclasses of this class,
+    only changing the __tablename__ attribute, and inheriting from db.Model (which
+    is required for the table to be created).
+    End result: each ChangingItem automatically has a changelog table/class created.
+    """
+
+    # Removes 'Change' part from class name
+    @declared_attr
+    def item_class_name(cls):
+        return cls.__name__[:-6]
+
+    # Removes '_change' part from table name
+    @declared_attr
+    def item_table_name(cls):
+        return cls.__tablename__[:-7]
+
+    # u_id : user id that made the change
+    @declared_attr
+    def u_id(cls):
+        return db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    
+    # user : User object corresponding to user that made the change
+    @declared_attr
+    def user(cls):
+        # backref creates <item>.changes attribute in user containing list of changes the user made
+        # for this type of item
+        return db.relationship('User', backref=cls.__tablename__ + 's')
+    
+    # p_id : project id that the item belongs to
+    @declared_attr
+    def p_id(cls):
+        return db.Column(db.Integer, 
+                db.ForeignKey(cls.item_table_name + '.p_id'), 
+                primary_key=True)
+    # i_id : item id that was changed
+    @declared_attr
+    def i_id(cls):
+        return db.Column(db.Integer,
+                db.ForeignKey(cls.item_table_name + '.id'),
+                primary_key=True)
+    
+    # item : Item object corresponding to item that was changed
+    @declared_attr
+    def item(cls):
+        # Somehow SQLAlchemy does not understand how to join two pairs of foreign keys
+        # So we have to spell it out for it in primary join
+        return db.relationship(cls.item_class_name, 
+                back_populates='changes',
+                primaryjoin='and_({0}.id=={1}.i_id, {0}.p_id=={1}.p_id)'.format(cls.item_class_name, cls.__name__))
+    
+    # id : the nth change made to this item
+    id = db.Column(db.Integer, primary_key=True)
+
+    # TODO: Enum for ChangeType
+    # Date and time when change was made
+    timestamp = db.Column(db.DateTime)
+    
+from src.models.item_models import ChangingItem
+
+# TODO: Should make sure subclasses are not abstract before creating changelog for them
+#       or alternatively, make sure they inherit db.Model
+Changes = [create_change_table(changing_item) for changing_item in AppUtil.get_all_subclasses(ChangingItem)]
+
+# TODO: Find a way to make a schema for the changes.
+# Problem: Using Change as model gives error since it does not have mapper,
+#       since it does not inherit db.Model
+# Idea: Use Abstract Concrete Class for Change so that it does inherit db.Model.
+# Problem with this idea is that it is hard
--- backend/src/models/item_models.py
+++ backend/src/models/item_models.py
@@ -1,9 +1,8 @@
 from src.models import db, ma
 from sqlalchemy.orm import declarative_mixin, declared_attr
-from src.app_util import AppUtil
 # Abstract Base Class, inheriting this makes a class abstract
 from abc import ABC
-# TODO: Enforce ProjectItem, ChangingItem and Change to be abstract
+# TODO: Enforce ProjectItem, ChangingItem to be abstract
 
 """
 This module includes project items, as well as relationships between those items.
@@ -14,19 +13,6 @@ Relevant info:
 @declarative_mixin : decorates a class as a sort of "abstract" class for tables
 @declared_attr : certain special attributes need to be declared as functions when using mixins
 """
-
-def create_change_table(cls):
-    """
-    For a ChangingItem with name 'Item' and table name 'item', this function is a class factory.
-    It returns a concrete Change class with name 'ItemChange' and table name 'item_change'.
-    This allows us to automatically create all the changelog tables for the items,
-    since all classes inheriting db.Model create a table.
-    """
-
-    # type function can create classes
-    return type(cls.__name__ + 'Change',  # class name
-            (Change, db.Model),  # base classes
-            {'__tablename__': cls.__tablename__ + '_change'})  # added attributes/methods
 
 @declarative_mixin
 class ProjectItem():
@@ -74,69 +60,6 @@ class ChangingItem(ProjectItem):
         return db.relationship(cls.change_class_name, 
                 back_populates='item',
                 primaryjoin='and_({0}.id=={1}.i_id, {0}.p_id=={1}.p_id)'.format(cls.__name__, cls.change_class_name))
-
-@declarative_mixin
-class Change():
-    """
-    The Changelog base class. We call this class change because an instance represents
-    a single change, not a log of changes.
-    A table is created by the declaration of a class. All changelogs are the same, but
-    require different tables. Thus we dynamically create subclasses of this class,
-    only changing the __tablename__ attribute, and inheriting from db.Model (which
-    is required for the table to be created).
-    End result: each ChangingItem automatically has a changelog table/class created.
-    """
-    # TODO: Use __table__args__ and ForeignKeyConstraint to specify join condition instead of primaryjoin
-
-    # Removes 'Change' part from class name
-    @declared_attr
-    def item_class_name(cls):
-        return cls.__name__[:-6]
-    # Removes '_change' part from table name
-    @declared_attr
-    def item_table_name(cls):
-        return cls.__tablename__[:-7]
-
-    # u_id : user id that made the change
-    @declared_attr
-    def u_id(cls):
-        return db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
-    
-    # user : User object corresponding to user that made the change
-    @declared_attr
-    def user(cls):
-        # backref creates <item>.changes attribute in user containing list of changes the user made
-        # for this type of item
-        return db.relationship('User', backref=cls.__tablename__ + 's')
-    
-    # p_id : project id that the item belongs to
-    @declared_attr
-    def p_id(cls):
-        return db.Column(db.Integer, 
-                db.ForeignKey(cls.item_table_name + '.p_id'), 
-                primary_key=True)
-    # i_id : item id that was changed
-    @declared_attr
-    def i_id(cls):
-        return db.Column(db.Integer,
-                db.ForeignKey(cls.item_table_name + '.id'),
-                primary_key=True)
-    
-    # item : Item object corresponding to item that was changed
-    @declared_attr
-    def item(cls):
-        # Somehow SQLAlchemy does not understand how to join two pairs of foreign keys
-        # So we have to spell it out for it in primary join
-        return db.relationship(cls.item_class_name, 
-                back_populates='changes',
-                primaryjoin='and_({0}.id=={1}.i_id, {0}.p_id=={1}.p_id)'.format(cls.item_class_name, cls.__name__))
-    
-    # id : the nth change made to this item
-    id = db.Column(db.Integer, primary_key=True)
-
-    # TODO: Enum for ChangeType
-    # Date and time when change was made
-    timestamp = db.Column(db.DateTime)
 
 class LabelType(ProjectItem, db.Model):
 
@@ -193,13 +116,28 @@ class Theme(ChangingItem, db.Model):
     # Boolean for if the theme was (soft) deleted (can be seen in history, but not used)
     deleted = db.Column(db.Boolean, default=False)
 
-# TODO: Should make sure subclasses are not abstract before creating changelog for them
-#       or alternatively, make sure they inherit db.Model
-Changes = [create_change_table(changing_item) for changing_item in AppUtil.get_all_subclasses(ChangingItem)]
+class Labelling(db.Model):
+
+    __tablename__ = 'labelling'
+    # The id of the user that labelled the artifact
+    u_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    # The id of the artifact that was labelled
+    a_id = db.Column(db.Integer, db.ForeignKey('artifact.id'), primary_key=True)
+    # The id of the label that the artifact was labelled with
+    l_id = db.Column(db.Integer, db.ForeignKey('label.id'), primary_key=True)
+    # The id of the project that the label/artifact belong to
+    p_id = db.Column(db.Integer, db.ForeignKey('project.id'), primary_key=True)
+    # Remark on why this label was chosen for this artifact
+    remark = db.Column(db.Text)
+    # When this artifact was labelled
+    timestamp = db.Column(db.DateTime)
+    # TODO: Add relationships, and consider backref
 
 # Note: This is a circular import, but not a circular dependency so nothing breaks
 # i.e., do not use this package at the top level
 from src.models.auth_models import User
+# Changes is a list with all the Change classes
+from src.models.change_models import Changes
 
 class LabelTypeSchema(ma.SQLAlchemyAutoSchema):
 
@@ -228,9 +166,3 @@ class ThemeSchema(ma.SQLAlchemyAutoSchema):
         model = Theme
         include_fk = True
         load_instance = True
-
-# TODO: Find a way to make a schema for the changes.
-# Problem: Using Change as model gives error since it does not have mapper,
-#       since it does not inherit db.Model
-# Idea: Use Abstract Concrete Class for Change so that it does inherit db.Model.
-# Problem with this idea is that it is hard
