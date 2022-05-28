50d8bbc53139af730f975fd75ae5b22aa372806f
Bartjan Henkemans <b.henkemans@student.tue.nl>
2022-05-28 22:39:32 +0200

    Implemented StringArtifact datatype

---
+++ frontend/src/app/stringartifact.spec.ts
@@ -0,0 +1,7 @@
+import { StringArtifact } from './stringartifact';
+
+describe('Artifact', () => {
+  it('should create an instance', () => {
+    expect(new StringArtifact()).toBeTruthy();
+  });
+});
