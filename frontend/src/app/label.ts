9bef33957af31fbc3ced35052da2d70207fd67f4
Bartjan Henkemans <b.henkemans@student.tue.nl>
2022-05-28 22:59:32 +0200

    Added name to labels

--- frontend/src/app/label.ts
+++ frontend/src/app/label.ts
@@ -3,6 +3,7 @@
  */
 export class Label {
     private labelID: string;
+    private labelName: string;
     private labelDescription: string;
     private nArtifact: number;
     private deleted: boolean;
@@ -20,16 +21,18 @@ export class Label {
      * @post this.labelID == labelID && this.labelDescription == labelDescription 
      *       && this.nArtifact == nArtifact && this.deleted == deleted
      */
-    constructor(labelID: string, labelDescription: string, nArtifact: number, deleted: boolean) {
+    constructor(labelID: string, labelName: string, labelDescription: string, nArtifact: number, deleted: boolean) {
         if (labelID.length <= 0){
             throw new Error("The labelID should not be of length 0 as an argument in the constructor");
         }
-
+        if(labelName.length <= 0) {
+            throw new Error("The labelName should not be of length 0 as an argument in the constructor");
+        }
         if (labelDescription.length <= 0) {
             throw new Error("The labelDescription should not be of length 0 as an argument in the constructor");
         }
-
         this.labelID = labelID;
+        this.labelName = labelName;
         this.labelDescription = labelDescription;
         this.nArtifact = nArtifact;
         this.deleted = deleted;
@@ -57,6 +60,27 @@ export class Label {
         this.labelID = labelID;
     }
 
+    /**
+     * Returns labelName
+     * @returns this.labelName
+     */
+    getLabelName(): string {
+        return this.labelName;
+    }
+
+    /**
+     * Sets labelName
+     * @param labelName: string 
+     * @post labelName.length > 0
+     * @throws {Error} if labelName.length <= 0 
+     * @post this.labelName == labelName
+     */
+    setLabelName(labelName: string): void {
+        if (labelName.length <= 0){
+            throw new Error("The labelName should not be of length 0 as an argument in setLabelID()");
+        }
+        this.labelName = labelName;
+    }
     /**
      * returns labelDescription
      * @returns this.labelDescription
