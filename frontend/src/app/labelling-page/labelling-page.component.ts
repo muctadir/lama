57ddd259ca43746ee8d2b1144bf50c774bca4e36
Bartjan Henkemans <b.henkemans@student.tue.nl>
2022-05-28 23:58:39 +0200

    Restored basic service of labeling page using types and a service

--- frontend/src/app/labelling-page/labelling-page.component.ts
+++ frontend/src/app/labelling-page/labelling-page.component.ts
@@ -1,83 +1,52 @@
-import { LoremIpsum } from "lorem-ipsum";
 import { Component, OnInit } from '@angular/core';
-import {NgbActiveModal, NgbModal} from '@ng-bootstrap/ng-bootstrap';
-import { LabelFormComponent } from 'app/label-form/label-form.component';
-
-const lorem = new LoremIpsum({
-  sentencesPerParagraph: {
-    max: 8,
-    min: 4
-  },
-  wordsPerSentence: {
-    max: 16,
-    min: 4
-  }
-});
-
-type labelType = {
-  labelTypeName: String,
-  labelTypeDescription: String,
-  labels: Array<label>
-}
-
-type label = {
-  labelName: String,
-  labelDescription: String,
-}
+import { NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import { LabelFormComponent } from '../label-form/label-form.component';
+import { LabelingDataService } from "app/labeling-data.service";
+import { StringArtifact } from 'app/stringartifact';
+import { LabelType } from 'app/label-type';
 
 @Component({
   selector: 'app-labelling-page',
   templateUrl: './labelling-page.component.html',
   styleUrls: ['./labelling-page.component.scss']
 })
-export class LabellingPageComponent implements OnInit {
-  artifactId: Number = 1;
-  labelers: Array<String> = ["Bartjan", "Veerle"]
-  artifact: String = lorem.generateParagraphs(10);
-  labelTypes: Array<labelType> = [
-    {
-      labelTypeName: "Type A",
-      labelTypeDescription: lorem.generateParagraphs(1),
-      labels: [{labelName: lorem.generateWords(1), labelDescription: lorem.generateSentences(5)},
-               {labelName: lorem.generateWords(1), labelDescription: lorem.generateSentences(5)},
-               {labelName: lorem.generateWords(1), labelDescription: lorem.generateSentences(5)},
-               {labelName: lorem.generateWords(1), labelDescription: lorem.generateSentences(5)},]
-    },
-    {
-      labelTypeName: "Type B",
-      labelTypeDescription: lorem.generateParagraphs(1),
-      labels: [{labelName: lorem.generateWords(1), labelDescription: lorem.generateSentences(5)},
-               {labelName: lorem.generateWords(1), labelDescription: lorem.generateSentences(5)},
-               {labelName: lorem.generateWords(1), labelDescription: lorem.generateSentences(5)},
-               {labelName: lorem.generateWords(1), labelDescription: lorem.generateSentences(5)},]
-    },
-  ]
+export class LabellingPageComponent implements OnInit{
 
+  artifact?: StringArtifact;
+  labels?: Array<LabelType>;
   hightlightedText: Selection | null = null;
 
-  notImplemented(){
-    alert("This button is not implemented.");
+  constructor(private modalService: NgbModal, 
+    private labelingDataService: LabelingDataService) { }
+
+  ngOnInit(): void {
+    this.getArtifact();
+    this.getLabels();
   }
 
+  getArtifact ():void {
+    this.labelingDataService.getArtifact()
+      .subscribe(artifact => {
+        artifact.addLabeler("Bartjan");
+        this.artifact = artifact;
+      });
+  }
+
+  getLabels (): void {
+    this.labelingDataService.getLabels()
+      .subscribe(labels => this.labels = labels);
+  }
+
+  
   selectedText() {
     this.hightlightedText = document.getSelection()
   }
-
-  splitArtifact(){
-    alert("Wowee, its a split");
-  }
-   
-  highlightArtifact(){
-    alert("Gee wizz, you did a highlight");
-  }
-
-  constructor(private modalService: NgbModal) { }
-
-  ngOnInit(): void {
-  }
-
+  
   open() {
     const modalRef = this.modalService.open(LabelFormComponent, { size: 'xl'});
   }
-
+  
+  notImplemented(){
+    alert("This button is not implemented.");
+  }
 }
