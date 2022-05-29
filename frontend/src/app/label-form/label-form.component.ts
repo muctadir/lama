4c94c8a4ae735f0acba6f011524da7b2ca347a68
Bartjan Henkemans <b.henkemans@student.tue.nl>
2022-05-29 14:57:41 +0200

    Implemented structure for submitting a new label tot the backend

--- frontend/src/app/label-form/label-form.component.ts
+++ frontend/src/app/label-form/label-form.component.ts
@@ -1,7 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+/**
+ * @author B. Henkemans
+ */
+import { Component, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { LabelingDataService } from 'app/labeling-data.service';
 import { LabelType } from 'app/label-type';
+import { FormControl } from '@angular/forms';
+import { Label } from 'app/label';
 @Component({
   selector: 'app-label-form',
   templateUrl: './label-form.component.html',
@@ -9,10 +14,13 @@ import { LabelType } from 'app/label-type';
 })
 export class LabelFormComponent implements OnInit {
   labelTypes?: Array<LabelType>;
-  @Input() labelType?: LabelType;
+  inputLabelType?: LabelType;
+  inputName?: string;
+  inputDescription?: string;
 
   constructor(public activeModal: NgbActiveModal, 
-    private labelingDataService: LabelingDataService) {}
+    private labelingDataService: LabelingDataService) {
+    }
 
   ngOnInit(): void {
     this.getLabels();
@@ -23,8 +31,32 @@ export class LabelFormComponent implements OnInit {
       .subscribe(labels => this.labelTypes = labels);
   }
 
-  notImplemented() {
-    alert("Not implemented");
+  submit (): void {
+    if(this.labelTypes === undefined 
+      || this.inputLabelType === undefined 
+      || this.inputName === undefined
+      || this.inputDescription === undefined
+      ) {
+        throw new Error ("Something went wrong.");
+      }
+      else {
+        for (var i: number = 0; i < this.labelTypes.length; i++) {
+          if (this.labelTypes[i].getLabelTypeName() === this.inputLabelType.getLabelTypeName()) {
+            this.labelTypes[i].addLabel(new Label(this.inputName, this.inputName, 
+              this.inputDescription, 0, false));
+              break;
+          }
+        }
+        this.labelingDataService.pushLabels(this.labelTypes);
+      }
+    
+    this.activeModal.close();
+  }
+  /**
+   * Error function for unimplemented features.
+   */
+  notImplemented(): void{
+    throw new Error("This function has not been implemented yet.");
   }
 
 }
