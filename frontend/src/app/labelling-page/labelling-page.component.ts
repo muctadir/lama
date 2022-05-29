dd46436c11748753818324e4d432e1aba93b4d6d
Bartjan Henkemans <b.henkemans@student.tue.nl>
2022-05-29 14:05:37 +0200

    Adding comments

--- frontend/src/app/labelling-page/labelling-page.component.ts
+++ frontend/src/app/labelling-page/labelling-page.component.ts
@@ -1,3 +1,6 @@
+/**
+ * @author B. Henkemans
+ */
 import { Component, OnInit } from '@angular/core';
 import { NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import { LabelFormComponent } from '../label-form/label-form.component';
@@ -12,18 +15,37 @@ import { LabelType } from 'app/label-type';
 })
 export class LabellingPageComponent implements OnInit{
 
+  /**
+   * artifact contains the artifact
+   * labels contains the label(types) connected to this project
+   * highlightedText contains a selection of the artifact
+   */
   artifact?: StringArtifact;
   labels?: Array<LabelType>;
-  hightlightedText: Selection | null = null;
+  hightlightedText: string = "";
 
+  /**
+   * Constructor passes in the modal service and the labeling data service
+   * @param modalService 
+   * @param labelingDataService 
+   */
   constructor(private modalService: NgbModal, 
     private labelingDataService: LabelingDataService) { }
 
+  /**
+   * ngOnInit runs after the constructor. When the constructor is executed
+   * the artifacts and labels are pulled in
+   */
   ngOnInit(): void {
     this.getArtifact();
     this.getLabels();
   }
 
+  /**
+   * Function which subscribes to the labelingDataService and retrieves the artifact.
+   * It waits for a response and when the response arrives it adds Bartjan 
+   * as a labeler and then puts the information into this.artifact.
+   */
   getArtifact ():void {
     this.labelingDataService.getArtifact()
       .subscribe(artifact => {
@@ -32,6 +54,10 @@ export class LabellingPageComponent implements OnInit{
       });
   }
 
+  /**
+   * Function which subscribes to the labelingDataService and retrieves labels.
+   * It waits for a response, when the response arrives it puts the information into this.labels.
+   */
   getLabels (): void {
     this.labelingDataService.getLabels()
       .subscribe(labels => this.labels = labels);
@@ -43,7 +69,7 @@ export class LabellingPageComponent implements OnInit{
    * of the artifact. If the selection is null or empty, the selection is set 
    * to ""
    */
-  selectedText() {
+  selectedText():void {
     var hightlightedText: Selection | null = document.getSelection();
     if (hightlightedText == null || hightlightedText.toString().length <= 0) {
       this.hightlightedText = ""
@@ -52,11 +78,17 @@ export class LabellingPageComponent implements OnInit{
     }
   }
   
-  open() {
+  /**
+   * Opens modal which contains the create LabelFormComponent.
+   */
+  open():void {
     const modalRef = this.modalService.open(LabelFormComponent, { size: 'xl'});
   }
   
-  notImplemented(){
-    alert("This button is not implemented.");
+  /**
+   * Error function for unimplemented features.
+   */
+  notImplemented(): void{
+    throw new Error("This function has not been implemented yet.");
   }
 }
