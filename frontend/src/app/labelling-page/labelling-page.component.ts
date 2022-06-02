a1c099eadb47bd0cf63b2afa50984b7c8758430e
Victoria <victoria.bogachenkova@gmail.com>
2022-06-02 19:09:56 +0200

    Changes based on merge comments

--- frontend/src/app/labelling-page/labelling-page.component.ts
+++ frontend/src/app/labelling-page/labelling-page.component.ts
@@ -25,8 +25,8 @@ export class LabellingPageComponent implements OnInit{
   artifact?: StringArtifact;
   labels?: Array<LabelType>;
   hightlightedText: string = "";
-  startChar?: number;
-  endChar?: number;
+  selectionStartChar?: number;
+  selectionEndChar?: number;
 
   /**
    * Constructor passes in the modal service and the labeling data service
@@ -74,21 +74,21 @@ export class LabellingPageComponent implements OnInit{
    * to ""
    */
   selectedText():void {
-    var hightlightedText: Selection | null = document.getSelection();
+    let hightlightedText: Selection | null = document.getSelection();
     //gets the start and end indices of the highlighted bit
-    var start: number = hightlightedText?.anchorOffset!;
-    var end: number = hightlightedText?.focusOffset!;
+    let startCharacter: number = hightlightedText?.anchorOffset!;
+    let endCharacter: number = hightlightedText?.focusOffset!;
     //make sure they in the right order
-    if (start > end) {
-      start = hightlightedText?.focusOffset!;
-      end = hightlightedText?.anchorOffset!;
+    if (startCharacter > endCharacter) {
+      startCharacter = hightlightedText?.focusOffset!;
+      endCharacter = hightlightedText?.anchorOffset!;
     }
     //put into global variable
-    this.startChar = start
-    this.endChar = end
+    this.selectionStartChar = startCharacter;
+    this.selectionEndChar = endCharacter;
     //this is so the buttons still pop up, idk if we need it so ill ask bartgang
     if (hightlightedText == null || hightlightedText.toString().length <= 0) {
-      this.hightlightedText = ""
+      this.hightlightedText = "";
     } else {
       this.hightlightedText = hightlightedText.toString();
     }
@@ -107,40 +107,40 @@ export class LabellingPageComponent implements OnInit{
    * Splitting function, gets text without splitting words and gets start and end char
    */
   split(): void {
-    var first = this.startChar!;
-    var last = this.endChar!;
-    first = this.posFixer(first, last)[0];
-    last = this.posFixer(first, last)[1] + 1;
-    var splitText = this.artifact?.data.substring(first, last);
-    alert("the text is: '" + splitText + "'\nthe start is at: "
-     + first + "\nthe end is at: " + last);
+    let firstCharacter = this.selectionStartChar!;
+    let lastCharacter = this.selectionEndChar!;
+    firstCharacter = this.posFixer(firstCharacter, lastCharacter)[0];
+    lastCharacter = this.posFixer(firstCharacter, lastCharacter)[1] + 1;
+    let splitText = this.artifact?.data.substring(firstCharacter, lastCharacter);
+    alert("The text is: '" + splitText + "'\nThe start is at: "
+     + firstCharacter + "\nThe end is at: " + lastCharacter);
   }
 
   /**
-   * Gets the correct indices so that words arent split
+   * Gets the correct indices so that words aren't split
    */
-  posFixer(start:number, end:number) {
+  posFixer(startPos:number, endPos:number) {
     //get the chars at index
-    var chart = this.artifact?.data.charAt(start);
-    var chend = this.artifact?.data.charAt(end - 1);
+    let chart = this.artifact?.data.charAt(startPos);
+    let chend = this.artifact?.data.charAt(endPos - 1);
     //if you just select the space
-    if (start - end == 1) {
-      return [start, end]
+    if (startPos - endPos == 1) {
+      return [startPos, endPos];
     }
     //else we move until we hit a space
-    while ( chart!= ' ' && start != -1) {
-      chart = this.artifact?.data.charAt(start);
-      start = start- 1;
+    while ( chart!= ' ' && startPos != -1) {
+      chart = this.artifact?.data.charAt(startPos);
+      startPos = startPos - 1;
     }
-    while (chend != ' ' && end != this.artifact?.data.length) {
-      chend = this.artifact?.data.charAt(end);
-      end++;
-      console.log(chend,end)
+    while (chend != ' ' && endPos != this.artifact?.data.length) {
+      chend = this.artifact?.data.charAt(endPos);
+      endPos++;
+      console.log(chend,endPos);
     }
     //last adjustements from going too far
-    start = start + 2;
-    end = end - 2;
-    return [start, end]; 
+    startPos = startPos + 2;
+    endPos = endPos - 2;
+    return [startPos, endPos]; 
   }
   
 
