a54919056dae187a397b863cfcb76eb7c3a8d05a
corvusumbra <chinnolavin@gmail.com>
2022-06-02 10:47:58 +0200

    Started Labels accordion view Work In Progress

--- frontend/src/app/single-theme-view/single-theme-view.component.ts
+++ frontend/src/app/single-theme-view/single-theme-view.component.ts
@@ -15,6 +15,17 @@ export class SingleThemeViewComponent implements OnInit {
   //HardCoded Sub-Themes
   subThemes = ['Happiness','Humor'];
 
+  //Hard Coded Labels
+  allLabels = [{labelName:'Happy',
+  labelDescription:'This label is used for any text that give off a general positive feeling of happiness or anything similar.'},
+  {labelName:'Laughter',
+  labelDescription:'This is laughter'},
+  {labelName:'Angry',
+  labelDescription:'This is anger'},
+  {labelName:'Depressing',
+  labelDescription:'This is depression'}];
+
+
 
   constructor() { }
 
