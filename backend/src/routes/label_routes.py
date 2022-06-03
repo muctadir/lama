5a3084c5e6618d06a0f2bdae0562d4c8640d74c4
Bartjan Henkemans <b.henkemans@student.tue.nl>
2022-06-03 19:50:01 +0200

    Added some final filtering

--- backend/src/routes/label_routes.py
+++ backend/src/routes/label_routes.py
@@ -1,3 +1,6 @@
+# Author: Eduardo
+# Author: Bartjan
+from crypt import methods
 from backend.src.app_util import check_args
 from src import db # need this in every route
 from flask import current_app as app
@@ -12,6 +15,7 @@ from src.models.item_models import Label, LabelSchema, LabelType, LabelTypeSchem
 
 label_routes = Blueprint("label", __name__, url_prefix="/label")
 
+# Author: Eduardo
 @label_routes.route('/create', methods=['POST'])
 @login_required
 def create_label(*, user):
@@ -20,23 +24,27 @@ def create_label(*, user):
 
     required = ('labelTypeId', 'labelName', 'labelDescription', 'pId')
 
+    # Check whether the required arguments are delivered
     if not check_args(required, args):
         return make_response('Bad Request', 400)
-
-    # TODO: Check formatting
-
+    # Check whether the length of the label name is at least one character long
+    if len(args['labelName']) <= 0:
+        return make_response('Bad request: Label name cannot have size <= 0')
+    # Check whether the length of label description is at least one character long
+    if len(args['labelDescription']) <= 0:
+        return make_response('Bad request: Label description cannot have size <= 0')
+    # Check whether the label type exists
     label_type = db.session.get(LabelType, args['labelTypeId'])
-
     if not label_type:
         return make_response('Label type does not exist', 400)
-    
+    # Check whether the label is part of the project
     if label_type.p_id != args['pId']:
         return make_response('Label type not in this project', 400)
-
+    # Make the label
     label = Label(name=args['labelName'],
         description=args['labelDescription'],
         lt_id=args['lt_id'])
-    
+    # Commit the label
     db.session.add(label)
     db.session.commit()
 
