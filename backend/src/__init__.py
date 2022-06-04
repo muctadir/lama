9ce8e2f9c8199c93e399398258a9ea4fc9802fce
Victoria <victoria.bogachenkova@gmail.com>
2022-06-04 13:02:33 +0200

    Added label_route
    Co-authored-by: Bartjan Henkemans <BHenkemans@users.noreply.github.com>

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -8,7 +8,7 @@ from src.models.auth_models import User
 import src.models.auth_models
 import src.models.project_models
 import src.models.item_models
-from src.routes import demos, auth_routes, project_routes, account_routes
+from src.routes import demos, auth_routes, project_routes, account_routes, label_routes
 from flask_cors import CORS
 from os import environ
 from pathlib import Path
@@ -115,6 +115,7 @@ def create_app(config={'TESTING': False}):
     app.register_blueprint(demos)
     app.register_blueprint(project_routes)
     app.register_blueprint(account_routes)
+    app.register_blueprint(label_routes)
 
     # Magic library that makes cross-origin resource sharing work.
     # TODO: Check if we are setting this up correctly.
