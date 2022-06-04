d335080c617023da7feaa9313841e0b7da7c1f61
Victoria <victoria.bogachenkova@gmail.com>
2022-06-04 12:06:36 +0200

    Declared route
    Co-authored-by: Bartjan Henkemans <BHenkemans@users.noreply.github.com>

--- backend/src/routes/__init__.py
+++ backend/src/routes/__init__.py
@@ -2,3 +2,4 @@ from src.routes.auth_routes import auth_routes
 from src.routes.demos import demos
 from src.routes.projects_routes import project_routes
 from src.routes.account_routes import account_routes
+from src.routes.label_routes import label_routes
