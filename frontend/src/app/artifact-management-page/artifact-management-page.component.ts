d2c3496107af06cf6408157c9a0da73f152f4b9d
hughjarc <64974431+hughjarc@users.noreply.github.com>
2022-06-05 11:28:02 +0200

    minor fix
    added back the open() function from other branch

--- frontend/src/app/artifact-management-page/artifact-management-page.component.ts
+++ frontend/src/app/artifact-management-page/artifact-management-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import { Router } from '@angular/router';
 import { ReroutingService } from 'app/rerouting.service';
+import { AddArtifactComponent } from 'app/add-artifact/add-artifact.component';
 
 
 @Component({
@@ -68,6 +69,10 @@ export class ArtifactManagementPageComponent {
    
 
   ];
+
+  open(){
+    const modalRef = this.modalService.open(AddArtifactComponent, { size: 'lg'});
+  }
 
   /**
    * Constructor passes in the modal service, initializes Router
