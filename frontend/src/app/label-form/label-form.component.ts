81191f8c6d05cc4f5ff040109a6d39385b7b1a21
Bartjan Henkemans <b.henkemans@student.tue.nl>
2022-06-05 15:21:53 +0200

    Implemented get all type function

--- frontend/src/app/label-form/label-form.component.ts
+++ frontend/src/app/label-form/label-form.component.ts
@@ -7,33 +7,43 @@ import { LabelingDataService } from 'app/labeling-data.service';
 import { LabelType } from 'app/classes/label-type';
 import { FormControl } from '@angular/forms';
 import { Label } from 'app/classes/label';
+import { ReroutingService } from 'app/rerouting.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-label-form',
   templateUrl: './label-form.component.html',
   styleUrls: ['./label-form.component.scss']
 })
 export class LabelFormComponent implements OnInit {
-  labelTypes?: Array<LabelType>;
+  routeService: ReroutingService;
+  url: string;
+  labelTypes: Array<LabelType>;
+
   inputLabelType?: LabelType;
   inputName?: string;
   inputDescription?: string;
 
-  constructor(public activeModal: NgbActiveModal, 
-    private labelingDataService: LabelingDataService) {
+  constructor(public activeModal: NgbActiveModal,
+    private labelingDataService: LabelingDataService,
+    private router: Router) {
+      this.labelTypes = new Array<LabelType>();
+      this.routeService = new ReroutingService();
+      this.url = this.router.url;
     }
 
   ngOnInit(): void {
-    this.getLabels();
+    const p_id = parseInt(this.routeService.getProjectID(this.url));
+    this.getLabelTypes(p_id);
   }
 
-  getLabels (): void {
-    // this.labelingDataService.getLabels()
-    //   .subscribe(labels => this.labelTypes = labels);
+  async getLabelTypes(p_id: number) : Promise<void> {
+    const labelTypes = await this.labelingDataService.getLabelTypes(p_id);
+    this.labelTypes = labelTypes;
   }
 
   submit (): void {
-    if(this.labelTypes === undefined 
-      || this.inputLabelType === undefined 
+    if(this.labelTypes === undefined
+      || this.inputLabelType === undefined
       || this.inputName === undefined
       || this.inputDescription === undefined
       ) {
@@ -42,14 +52,14 @@ export class LabelFormComponent implements OnInit {
       else {
         for (var i: number = 0; i < this.labelTypes.length; i++) {
           if (this.labelTypes[i].getName() === this.inputLabelType.getName()) {
-            this.labelTypes[i].addLabel(new Label(1, this.inputName, this.inputName, 
+            this.labelTypes[i].addLabel(new Label(1, this.inputName, this.inputName,
               this.inputDescription));
               break;
           }
         }
         // this.labelingDataService.pushLabels(this.labelTypes);
       }
-    
+
     this.activeModal.close();
   }
   /**
--- frontend/src/app/labeling-data.service.ts
+++ frontend/src/app/labeling-data.service.ts
@@ -27,19 +27,18 @@ export class LabelingDataService {
     if (p_id < 0) throw new Error("p_id cannot be less than 0")
     // Array with results
     let result: Array<Label> = new Array<Label>();
-    // Actual request 
+    // Actual request
     return axios.get('http://127.0.0.1:5000/label/getAll', {
       headers: {
         'u_id_token': token
-      }, 
+      },
       params: {
         'p_id': p_id
       }
     }).then((response) => {
       // Put response in object
       response.data.forEach((d: any) => {
-        console.log(d.label.id)
-        result.push(new Label(d.label.id, d.label.name, 
+        result.push(new Label(d.label.id, d.label.name,
         d.label.description, d.label_type));
       });
       // return result
@@ -50,6 +49,12 @@ export class LabelingDataService {
     });
   }
 
+  /**
+   *
+   * @param p_id
+   * @param label_id
+   * @returns
+   */
   getLabel(p_id: number, label_id: number): Promise<Label> {
     let token: string | null  = sessionStorage.getItem('ses_token');
     // Check if the session token exists
@@ -62,18 +67,45 @@ export class LabelingDataService {
     return axios.get('http://127.0.0.1:5000/label/get', {
       headers: {
         'u_id_token': token
-      }, 
+      },
       params: {
         'p_id': p_id,
         'label_id': label_id
       }
     }).then((response) => {
-      return new Label(response.data.label.id, response.data.label.name, 
+      return new Label(response.data.label.id, response.data.label.name,
         response.data.label.description, response.data.label_type);
-    })
+    });
   }
 
-  
+  /**
+   *
+   */
+  getLabelTypes(p_id: number): Promise<Array<LabelType>> {
+    let token: string | null  = sessionStorage.getItem('ses_token');
+    // Check if the session token exists
+    if (typeof token !== "string") throw new Error("User is not logged in");
+    // Check if the p_id is larger than 0
+    if (p_id < 0) throw new Error("p_id cannot be less than 0")
+    let result: Array<LabelType> = new Array<LabelType>();
+    return axios.get('http://127.0.0.1:5000/labeltype/getAll', {
+      headers: {
+        'u_id_token': token
+      },
+      params: {
+        'p_id': p_id,
+      }
+    }).then((response) => {
+      response.data.forEach((d: any) => {
+        result.push(new LabelType(d.id, d.name, new Array<Label>()))
+      });
+      return result;
+    }).catch(err => {
+      throw err;
+    });
+
+  }
+
   pushLabels(newLabels: Array<LabelType>): void{
     console.log("Pushing to backend...")
     console.log(newLabels)
