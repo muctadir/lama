b908ce15d9fbd4697c064a72b1739cd5e8b5a6f1
JarlJansen123 <jarljansen123@gmail.com>
2022-06-06 13:57:30 +0200

    fixed test case dependency issue
    fixed the dependency issue

--- frontend/src/app/single-theme-view/single-theme-view.component.spec.ts
+++ frontend/src/app/single-theme-view/single-theme-view.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RouterTestingModule } from '@angular/router/testing';
+import { NgbAccordion } from '@ng-bootstrap/ng-bootstrap';
 
 import { SingleThemeViewComponent } from './single-theme-view.component';
 
@@ -10,7 +11,8 @@ describe('SingleThemeViewComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SingleThemeViewComponent ],
+      // Adding the NgbAccordion dependency, unsure why this needs to be imported for the test case
+      declarations: [ SingleThemeViewComponent, NgbAccordion ],
       // Adding the RouterTestingModule dependency
       imports: [RouterTestingModule]
     })
