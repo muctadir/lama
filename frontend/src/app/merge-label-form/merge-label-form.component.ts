adb0d428bdccbed525b4d8d5aa5984a630bc83f6
Victoria <victoria.bogachenkova@gmail.com>
2022-06-07 14:27:15 +0200

    Merge label changes in html

--- frontend/src/app/merge-label-form/merge-label-form.component.ts
+++ frontend/src/app/merge-label-form/merge-label-form.component.ts
@@ -3,6 +3,7 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 // Tyep for label
 type label = {
+  labelId: Number,
   labelName: String,
   labelDescription: String,
   labelType: String
@@ -13,12 +14,25 @@ type label = {
   templateUrl: './merge-label-form.component.html',
   styleUrls: ['./merge-label-form.component.scss']
 })
-export class MergeLabelFormComponent implements OnInit {
+export class MergeLabelFormComponent{
 
   // Dummy variables
   labels: Array<label> = [
     {
+      labelId: 1,
       labelName: "Type A",
+      labelDescription: "Nullam gravida enim et ipsum feugiat, lobortis tempus quam facilisis. Phasellus neque lacus, tincidunt non sollicitudin at, mattis id ante. Nullam efficitur scelerisque sem, sit amet pharetra orci pellentesque a. Donec ullamcorper leo eu sagittis dictum. Mauris ut est nisi. Sed sed felis justo. Quisque at ligula quis arcu pretium malesuada. Sed a rutrum felis. Quisque finibus ipsum libero, id lacinia enim varius ullamcorper. Nullam scelerisque dolor nulla, in laoreet libero commodo at. Nunc non lacus at felis maximus sodales sed eu lorem. Integer non cursus felis. Cras vel ornare arcu. Pellentesque finibus at metus vel suscipit. Ut dignissim dictum semper. Pellentesque nec dignissim ex.",
+      labelType: "Lorem"
+    },
+    {
+      labelId: 2,
+      labelName: "Type B",
+      labelDescription: "Nullam gravida enim et ipsum feugiat, lobortis tempus quam facilisis. Phasellus neque lacus, tincidunt non sollicitudin at, mattis id ante. Nullam efficitur scelerisque sem, sit amet pharetra orci pellentesque a. Donec ullamcorper leo eu sagittis dictum. Mauris ut est nisi. Sed sed felis justo. Quisque at ligula quis arcu pretium malesuada. Sed a rutrum felis. Quisque finibus ipsum libero, id lacinia enim varius ullamcorper. Nullam scelerisque dolor nulla, in laoreet libero commodo at. Nunc non lacus at felis maximus sodales sed eu lorem. Integer non cursus felis. Cras vel ornare arcu. Pellentesque finibus at metus vel suscipit. Ut dignissim dictum semper. Pellentesque nec dignissim ex.",
+      labelType: "Ipsum"
+    },
+    {
+      labelId: 3,
+      labelName: "Type C",
       labelDescription: "Nullam gravida enim et ipsum feugiat, lobortis tempus quam facilisis. Phasellus neque lacus, tincidunt non sollicitudin at, mattis id ante. Nullam efficitur scelerisque sem, sit amet pharetra orci pellentesque a. Donec ullamcorper leo eu sagittis dictum. Mauris ut est nisi. Sed sed felis justo. Quisque at ligula quis arcu pretium malesuada. Sed a rutrum felis. Quisque finibus ipsum libero, id lacinia enim varius ullamcorper. Nullam scelerisque dolor nulla, in laoreet libero commodo at. Nunc non lacus at felis maximus sodales sed eu lorem. Integer non cursus felis. Cras vel ornare arcu. Pellentesque finibus at metus vel suscipit. Ut dignissim dictum semper. Pellentesque nec dignissim ex.",
       labelType: "Lorem"
     }
@@ -26,8 +40,6 @@ export class MergeLabelFormComponent implements OnInit {
 
   // Constructor modal
   constructor(public activeModal: NgbActiveModal) {}
-
-  ngOnInit(): void { }
 
   // Not implemented function
   notImplemented() {
