0c9427d80691088d51cfab16c5ac7fe9ab463b99
eduardo-devintage <63951752+eduardo-devintage@users.noreply.github.com>
2022-06-09 13:06:38 +0200

    better user conflicts
    can still be improved

--- backend/src/routes/project_routes.py
+++ backend/src/routes/project_routes.py
@@ -329,14 +329,12 @@ def nr_project_conflicts(p_id):
 
     return result
 
-def nr_user_conflicts(u_id):
-    
-    # Artifacts the user has labelled
-    labelled = select(
-        Labelling.a_id 
-    ).where(
-        Labelling.u_id == u_id
-    ).subquery()
+"""
+@returns a dictionary indexed by user id mapping to the number of conflicts 
+         the user has in the project given by p_id. A user with 0 conflicts
+         is not indexed.
+"""
+def nr_user_conflicts(p_id):
 
     # Number of differing labels per label type and artifact
     per_label_type = select(
@@ -344,7 +342,7 @@ def nr_user_conflicts(u_id):
         Labelling.lt_id, # Label type id (can get rid of this maybe?)
         func.count(distinct(Labelling.l_id)).label('label_count') # Distinct labels for a label type (renamed to 'label_count')
     ).where(
-        Labelling.a_id == labelled.c.a_id # The artifact was labelled by the user
+        Labelling.p_id == p_id
     ).group_by(
         Labelling.a_id, # Grouped by artifacts and
         Labelling.lt_id # by label type
@@ -360,13 +358,25 @@ def nr_user_conflicts(u_id):
         per_label_type.c.a_id # Grouped by artifact
     ).subquery()
 
+    per_user_artifact = select(
+        Labelling.u_id, Labelling.a_id, per_artifact.c.conflict_count.label('conflict_count')
+    ).where(
+        Labelling.a_id == per_artifact.c.a_id
+    ).group_by(
+        Labelling.a_id, Labelling.u_id
+    ).subquery()
+
     per_user = select(
-        func.sum(per_artifact.c.conflict_count) # Sum conflicts across all artifacts
+        per_user_artifact.c.u_id, func.sum(per_user_artifact.c.conflict_count)
+    ).group_by(
+        per_user_artifact.c.u_id
     )
 
-    result = db.session.scalar(per_user)
+    results = db.session.execute(per_user).all()
 
-    if not result:
-        result = 0
+    # TODO: Add zeros automatically
+    results = dict(
+        (u_id, int(conflicts)) for u_id, conflicts in results
+    )
 
-    return result
+    print(results)
