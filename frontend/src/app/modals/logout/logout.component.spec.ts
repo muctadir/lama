e63b1aa3931bd625d778a0a7cf0eabd8fec979cc
Ana-Maria-O <anamaria.olteniceanu@gmail.com>
2022-06-11 22:43:40 +0200

    Logout component creation test passes

--- frontend/src/app/modals/logout/logout.component.spec.ts
+++ frontend/src/app/modals/logout/logout.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 import { LogoutComponent } from './logout.component';
 
@@ -8,14 +11,14 @@ describe('LogoutComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LogoutComponent ]
+      imports: [RouterTestingModule],
+      declarations: [ LogoutComponent ],
+      providers: [NgbActiveModal]
     })
     .compileComponents();
-  });
-
-  beforeEach(() => {
     fixture = TestBed.createComponent(LogoutComponent);
     component = fixture.componentInstance;
+    // component.activeModal = new NgbActiveModal();
     fixture.detectChanges();
   });
 
