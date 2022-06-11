258e96d61c0199c91250f3744fb0808c0cc43432
Ana-Maria-O <anamaria.olteniceanu@gmail.com>
2022-06-11 13:41:15 +0200

    Updated backend (will finish after requestHandler)

--- backend/src/routes/project_routes.py
+++ backend/src/routes/project_routes.py
@@ -9,8 +9,9 @@ from src.models.auth_models import User, UserSchema, UserStatus, SuperAdmin
 from src.models.item_models import Artifact, LabelType
 from src.models.project_models import Membership, ProjectSchema
 from flask import jsonify, Blueprint, make_response, request
-from sqlalchemy import select
+from sqlalchemy import select, func
 from src.app_util import check_args, login_required
+from sqlalchemy.exc import OperationalError
 
 project_routes = Blueprint("project", __name__, url_prefix="/project")
 
@@ -30,17 +31,15 @@ For getting the project information
 def home_page(*, user):
 
     # Get membership of the user
-    projects_of_user = db.session.execute(
+    projects_of_user = db.session.scalars(
         select(Membership).where(Membership.u_id==user.id)
-    ).scalars().all()
+    ).all()
 
     # List for project information
     projects_info = []
 
     # Schema to serialize the Project
     project_schema = ProjectSchema()
-    # Schema to serialize the User
-    user_schema = UserSchema()
 
     # For loop for admin, users, #artifacts
     for membership_project in projects_of_user:
@@ -58,23 +57,17 @@ def home_page(*, user):
         projects_admin = membership_project.admin
 
         # Get the artifacts for each project 
-        project_artifacts_stmt = select(Artifact).where(Artifact.p_id==project_id)
-        project_artifacts = db.session.execute(project_artifacts_stmt).scalars().all()
+        project_artifacts_stmt = select(func.count(Artifact.id)).where(Artifact.p_id==project_id)
+        project_artifacts = db.session.scalar(project_artifacts_stmt)
         # Get the number of total artifacts
-        project_nr_artifacts = len(project_artifacts)
+        project_nr_artifacts = project_artifacts
         # Get the number of completely labelled artifacts for each project
-        project_nr_cl_artifacts = len(db.session.execute(
+        project_nr_cl_artifacts = db.session.scalar(
             project_artifacts_stmt.where(Artifact.completed==True)
-        ).scalars().all())
+        )
 
-        # Get the users in the project
-        project_users = project.users
-        # Serialize all users
-        users = []  
-        for user in project_users:
-            user_dumped = user_schema.dump(user)
-            user_dumped.pop("password")
-            users.append(user_dumped)
+        # Get the serialized users in the project
+        users = get_serialized_users(project.users)
         
         # Put all values into a dictonary
         info = {
@@ -102,15 +95,12 @@ For getting all users that can be added to a project
 def get_users(*, user):
 
     # Make a list of all approved users
-    all_users = db.session.execute(select(User).where(User.status==UserStatus.approved,
+    all_users = db.session.scalars(select(User).where(User.status==UserStatus.approved,
             User.type != 'super_admin',
-            User.id != user.id)).scalars().all()
-
-    # Schema to serialize the User
-    user_schema = UserSchema()
+            User.id != user.id)).all()
 
     # Convert the list of users to json
-    response_users = jsonify(user_schema.dump(all_users, many=True))
+    response_users = jsonify(get_serialized_users(all_users))
 
     # Return the list of users
     return make_response(response_users)
@@ -121,7 +111,7 @@ For creating a new project
 @project_routes.route("/creation", methods=["POST"])
 @login_required
 def create_project(*, user):
-
+    # TODO: Update how to get project_info once requestHandler is used
     # Get the information given by the frontend
     project_info = request.json
 
@@ -131,7 +121,6 @@ def create_project(*, user):
 
     # Add the project to the database
     db.session.add(project)
-    db.session.commit()
     
     # Get the ids of all users 
     users = project_info["users"]
@@ -152,8 +141,6 @@ def create_project(*, user):
             'u_id' : super_admin_id[0],
             'admin' : True
         })
-    # Commit the users
-    db.session.commit()
 
     # Get the label types from frontend
     type_names = project_info["labelTypes"]
@@ -164,5 +151,32 @@ def create_project(*, user):
         
         # Add the label type to the database
         db.session.add(label_type)
+
+    # Try committing all changes to the database   
+    try:
         db.session.commit()
+    except OperationalError:
+        return make_response('Internal Server Error', 503)
+    
     return make_response('OK', 200)
+
+"""
+Function to get a serialized list of users
+@param users a list of type User, the list of users to be serialized
+@returns a list of serialized users
+"""
+def get_serialized_users(users):
+    # Schema to serialize the Users
+    user_schema = UserSchema()
+
+     # Serialize all users
+    users_list = []  
+    for user in users:
+        # Serialize the user
+        user_dumped = user_schema.dump(user)
+        # Pop the password of the user
+        user_dumped.pop("password")
+        # Add the serialized user to the list of serialized users
+        users_list.append(user_dumped)
+    
+    return users_list
