0a8fbecc2bb2c07b69d042537a30dd0eed7d780f
Ana-Maria-O <anamaria.olteniceanu@gmail.com>
2022-06-12 16:28:29 +0200

    Test cases for stats.component and stats-data.service

--- frontend/src/app/stats/stats.component.spec.ts
+++ frontend/src/app/stats/stats.component.spec.ts
@@ -1,20 +1,41 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { StatsDataService } from 'app/services/stats-data.service';
 import { RouterTestingModule } from '@angular/router/testing';
-
+import { Router } from '@angular/router';
 import { StatsComponent } from './stats.component';
+import { Project } from 'app/classes/project';
 
 describe('StatsComponent', () => {
+  /* Objects to be used in testing */
+  // Project id
+  const p_id = 5;
+  // Project
+  const project = new Project(5, "Example", "Tester project")
+  // Project data
+  const project_data = {
+    'project_data': project,
+    'conflicts': 10
+  };
+  // User statistics
+  const user_contribution = ['Some', 'Data'];
+
   let component: StatsComponent;
   let fixture: ComponentFixture<StatsComponent>;
 
+  // Instantiation of StatsDataService
+  let statsDataService: StatsDataService;
+  // Instantiation of Router
+  let router: Router;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ StatsComponent ],
+      declarations: [StatsComponent],
       // Adding the RouterTestingModule dependency
       imports: [RouterTestingModule],
     })
-    .compileComponents();
+      .compileComponents();
+    statsDataService = TestBed.inject(StatsDataService);
+    router = TestBed.inject(Router);
   });
 
   beforeEach(() => {
@@ -26,5 +47,55 @@ describe('StatsComponent', () => {
   // Checks whether the component is created successfully
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  // Checks if getProject function works correctly
+  it('Tests getProject()', async () => {
+    // Return project_data when statsDataService.getProject(p_id) is called
+    spyOn(statsDataService, 'getProject').and
+      .returnValue(Promise.resolve(project_data));
+
+    // Call the getProject function
+    await component.getProject(p_id);
+
+    // Check that statsDataService.getProject(p_id) 
+    // was called with the right p_id
+    expect(statsDataService.getProject).toHaveBeenCalledWith(p_id);
+    // Check that project has the right data
+    expect(component.project).toEqual(project_data['project_data'])
+    // Check that conflicts has the right data
+    expect(component.conflicts).toEqual(project_data['conflicts'])
+  });
+
+  // Checks if getUserStats function works correctly
+  it('Tests getUserStats()', async () => {
+    // Return user_contribution when 
+    // statsDataService.getUserStats(p_id) is called
+    spyOn(statsDataService, 'getUserStats').and
+      .returnValue(Promise.resolve(user_contribution));
+
+    // Call the getUserStats function
+    await component.getUserStats(p_id);
+
+    // Check that statsDataService.getUserStats(p_id) 
+    // was called with the right p_id
+    expect(statsDataService.getUserStats).toHaveBeenCalledWith(p_id);
+    // Check that user_contribution has the right data
+    expect(component.user_contribution).toEqual(user_contribution)
+  });
+
+  it('Tests reRouter()', () => {
+    // When router.url gets called, return this string
+    spyOnProperty(router, 'url', 'get').and.returnValue('/project/5/stats');
+
+    // Spy on router.navigate and stub the call
+    spyOn(router, 'navigate');
+
+    // Call the reRoute function
+    component.reRouter();
+
+    // Check if reRouter navigates to the labelling page
+    expect(router.navigate).toHaveBeenCalledWith(['/project', '5', 'labelling-page']);
+
   });
 });
