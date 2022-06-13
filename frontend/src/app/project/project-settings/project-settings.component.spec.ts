147260db36f2b4df06cfba5220de5de495b9f59b
Ana-Maria-O <anamaria.olteniceanu@gmail.com>
2022-06-13 12:30:09 +0200

    More test fixing

--- frontend/src/app/project/project-settings/project-settings.component.spec.ts
+++ frontend/src/app/project/project-settings/project-settings.component.spec.ts
@@ -1,16 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
 
 import { ProjectSettingsComponent } from './project-settings.component';
+import { FormBuilder } from '@angular/forms';
 
 describe('ProjectSettingsComponent', () => {
   let component: ProjectSettingsComponent;
   let fixture: ComponentFixture<ProjectSettingsComponent>;
 
+  // Instantiation of Router
+  let router: Router;
+
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProjectSettingsComponent ]
+      declarations: [ProjectSettingsComponent],
+      imports: [RouterTestingModule],
+      providers: [FormBuilder]
     })
-    .compileComponents();
+      .compileComponents();
+    router = TestBed.inject(Router);
   });
 
   beforeEach(() => {
