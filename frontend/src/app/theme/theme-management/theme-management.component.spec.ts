556cd5436bfeb249660ee7482b8a3379097df7fe
TheVeerloes <veerlefurst@gmail.com>
2022-06-14 10:49:07 +0200

    Fixed enters management page

--- frontend/src/app/theme/theme-management/theme-management.component.spec.ts
+++ frontend/src/app/theme/theme-management/theme-management.component.spec.ts
@@ -1,10 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
-
 import { RouterTestingModule } from '@angular/router/testing';
-import { Theme } from 'app/classes/theme';
-
 import { ThemeManagementComponent } from './theme-management.component';
 
 describe('ThemeManagementComponent', () => {
@@ -38,16 +35,12 @@ describe('ThemeManagementComponent', () => {
   it('Tests the reRouter function to createTheme page', () => {
     // Set p_id in component
     component.p_id = 5;
-
     // Create spy for get url call
     spyOnProperty(router, 'url', 'get').and.returnValue('/project/'+component.p_id+'/createTheme');
-
      // Create spy on the router.navigate function, and stubs the call (doesnt do anything)
-    spyOn(router, 'navigate');
-    
+    spyOn(router, 'navigate');    
     // Calls the changePage function
-    component.reRouter("createTheme");
-    
+    component.reRouter("createTheme");    
     // Checks whether the function works properly
     expect(router.navigate).toHaveBeenCalledWith(['/project', component.p_id, 'createTheme']);
   });
@@ -56,31 +49,24 @@ describe('ThemeManagementComponent', () => {
   it('Tests the reRouter function to a single theme page', () => {
     // Set p_id in component
     component.p_id = 5;
-
     // Create spy for get url call
     spyOnProperty(router, 'url', 'get').and.returnValue('/project/'+component.p_id+'/singleTheme/4');
-
      // Create spy on the router.navigate function, and stubs the call (doesnt do anything)
-    spyOn(router, 'navigate');    
-    
+    spyOn(router, 'navigate');        
     // Calls the changePage function
-    component.reRouterTheme("singleTheme", 4);
-    
+    component.reRouterTheme("singleTheme", 4);    
     // Checks whether the function works properly
     expect(router.navigate).toHaveBeenCalledWith(['/project', component.p_id, 'singleTheme', 4]);
   });
 
   // Test the changePage function
   it('Tests if the get_theme_management_info function is called on initialization', () => {    
-
     // Create spy for get url call
     let spy = spyOn(component, 'get_theme_management_info');
     // Call ngOnInit
-    component.ngOnInit();
-    
+    component.ngOnInit();    
     // Checks whether the function is called in ngOnInit
     expect(spy).toHaveBeenCalled();
   });
-
 
 });
