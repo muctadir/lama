8c795c05f29aa9df751035f6b89cf000d26fe432
Ana-Maria-O <anamaria.olteniceanu@gmail.com>
2022-06-21 14:10:18 +0200

    List comprehension

--- backend/src/routes/labelling_routes.py
+++ backend/src/routes/labelling_routes.py
@@ -125,16 +125,10 @@ def edit_labelling(*, user, membership):
         return make_response('Bad Request', 400)
     
     #Array to hold updated labellings
-    updated_labellings = []
-    
-    test = [{'u_id': labelling['u_id'],'a_id': args['a_id'], 'lt_id': labelling['lt_id'],
-                     'l_id': labelling['id']} for labelling in args['labellings']]
-    print(test)
-
-    #Appending updated information about each labelling to the list above
-    for key in args['labellings']:
-        updated_labellings.append({'u_id': args['labellings'][key]['u_id'],'a_id': args['a_id'], 'lt_id': args['labellings'][key]['lt_id'],
-                    'l_id': args['labellings'][key]['id']})       
+    updated_labellings = [
+        {'u_id': args['labellings'][key]['u_id'],'a_id': args['a_id'], 'lt_id': args['labellings'][key]['lt_id'],
+                    'l_id': args['labellings'][key]['id']} for key in args['labellings']
+    ]
 
     print(updated_labellings)
     # Get project with supplied ID
