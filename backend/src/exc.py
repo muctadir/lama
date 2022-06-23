5337898b5302cb02ede75217cc7b92a95f5491be
eduardo-devintage <63951752+eduardo-devintage@users.noreply.github.com>
2022-06-23 08:58:52 +0200

    imported my changes

--- backend/src/exc.py
+++ backend/src/exc.py
@@ -6,3 +6,10 @@ This file contains exceptions we use for the app
 
 class ChangeSyntaxError(Exception):
     pass
+
+class ThemeCycleDetected(Exception):
+
+    # This number represents the error code for recursion limit in a MySQLdb database.
+    MySQLdb = 3636
+    # This number represents the error code for recursion limit in a MariaDB database.
+    MariaDB = 1456
