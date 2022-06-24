788510fa7c7c740d93ffd4cd87aa5f4d1a145caa
Bartjan Henkemans <b.henkemans@student.tue.nl>
2022-06-24 23:43:30 +0200

    Slight refactoring

--- backend/src/tests/test_label.py
+++ backend/src/tests/test_label.py
@@ -4,7 +4,6 @@ from sqlalchemy import select
 from src.models.item_models import Label
 from src import db
 from src.conftest import RequestHandler
-from jwt import encode
 
 
 def test_create(app, client):
@@ -12,102 +11,102 @@ def test_create(app, client):
         In this test, the create label route is tested.
     """
 
-    with app.app_context():
-        label = {
-            'labelTypeId': 3,
-            'labelName': 'Good idea',
-            'labelDescription': 'This is an example of a good idea!',
-            'p_id': 2
-        }
-        # Incorrect method
-        response = client.get('/label/create')
-        assert response.status_code == 405
+    label = {
+        'labelTypeId': 3,
+        'labelName': 'Good idea',
+        'labelDescription': 'This is an example of a good idea!',
+        'p_id': 2
+    }
+    # Incorrect method
+    response = client.get('/label/create')
+    assert response.status_code == 405
 
-        # Incorrect method
-        response = client.patch('/label/create')
-        assert response.status_code == 405
+    # Incorrect method
+    response = client.patch('/label/create')
+    assert response.status_code == 405
 
-        # Non-existent user
-        request_handler_empty = RequestHandler(app, client, '')
-        create_label_helper(request_handler_empty, label,
-                            'User does not exist', 401)
+    # Non-existent user
+    request_handler_empty = RequestHandler(app, client, '')
+    create_label_helper(request_handler_empty, label,
+                        'User does not exist', 401)
 
-        # User not part of project
-        request_handler_wrong_project = RequestHandler(app, client, 7)
-        create_label_helper(request_handler_wrong_project,
-                            label, 'Unauthorized', 401)
+    # User not part of project
+    request_handler_wrong_project = RequestHandler(app, client, 7)
+    create_label_helper(request_handler_wrong_project,
+                        label, 'Unauthorized', 401)
 
-        # Setting up authorized user
-        request_handler = RequestHandler(app, client, 2)
+    # Setting up authorized user
+    request_handler = RequestHandler(app, client, 2)
 
-        # Request missing information, missing p_id
-        create_label_helper(request_handler, {},
-                            'Bad Request, missing p_id', 400)
+    # Request missing information, missing p_id
+    create_label_helper(request_handler, {},
+                        'Bad Request, missing p_id', 400)
 
-        # Request missing information, p_id included
-        label_missing_information = {'labelTypeId': 3,
-                                     'labelDescription': 'This is an example of a good idea!',
-                                     'p_id': 2}
-        create_label_helper(
-            request_handler, label_missing_information, 'Bad Request', 400)
+    # Request missing information, p_id included
+    label_missing_information = {'labelTypeId': 3,
+                                 'labelDescription': 'This is an example of a good idea!',
+                                 'p_id': 2}
+    create_label_helper(
+        request_handler, label_missing_information, 'Bad Request', 400)
 
-        # Request with invalid character in the name
-        label_invalid_character = {'labelTypeId': 3,
-                                   'labelName': 'Good idea\\',
-                                   'labelDescription': 'This is an example of a good idea!',
-                                   'p_id': 2}
+    # Request with invalid character in the name
+    label_invalid_character = {'labelTypeId': 3,
+                               'labelName': 'Good idea\\',
+                               'labelDescription': 'This is an example of a good idea!',
+                               'p_id': 2}
 
-        create_label_helper(
-            request_handler, label_invalid_character, 'Input contains a forbidden character', 511)
+    create_label_helper(
+        request_handler, label_invalid_character, 'Input contains a forbidden character', 511)
 
-        # Request with empty name
-        label_empty_name = {'labelTypeId': 3,
-                            'labelName': '',
-                            'labelDescription': 'This is an example of a good idea!',
-                            'p_id': 2}
+    # Request with empty name
+    label_empty_name = {'labelTypeId': 3,
+                        'labelName': '',
+                        'labelDescription': 'This is an example of a good idea!',
+                        'p_id': 2}
 
-        create_label_helper(
-            request_handler, label_empty_name, 'Label name cannot be empty', 400)
+    create_label_helper(
+        request_handler, label_empty_name, 'Label name cannot be empty', 400)
 
-        # Request with empty description
-        label_empty_name = {'labelTypeId': 3,
-                            'labelName': 'Good idea',
-                            'labelDescription': '',
-                            'p_id': 2}
+    # Request with empty description
+    label_empty_name = {'labelTypeId': 3,
+                        'labelName': 'Good idea',
+                        'labelDescription': '',
+                        'p_id': 2}
 
-        create_label_helper(
-            request_handler, label_empty_name, 'Label description cannot be empty', 400)
+    create_label_helper(
+        request_handler, label_empty_name, 'Label description cannot be empty', 400)
 
-        # Request with taken name
-        label_duplicate = {'labelTypeId': 3,
-                           'labelName': 'Boring idea',
-                           'labelDescription': 'just boring',
-                           'p_id': 2}
+    # Request with taken name
+    label_duplicate = {'labelTypeId': 3,
+                       'labelName': 'Boring idea',
+                       'labelDescription': 'just boring',
+                       'p_id': 2}
 
-        create_label_helper(
-            request_handler, label_duplicate, 'Label name already exists', 400)
+    create_label_helper(
+        request_handler, label_duplicate, 'Label name already exists', 400)
 
-        # Request with labelTypeId \not\in project
-        label_wrong_lt = {'labelTypeId': 2,
-                          'labelName': 'Brilliant idea',
-                          'labelDescription': 'A brilliant idea!',
-                          'p_id': 2}
-        create_label_helper(
-            request_handler, label_wrong_lt, 'Label type not in this project', 400)
+    # Request with labelTypeId \not\in project
+    label_wrong_lt = {'labelTypeId': 2,
+                      'labelName': 'Brilliant idea',
+                      'labelDescription': 'A brilliant idea!',
+                      'p_id': 2}
+    create_label_helper(
+        request_handler, label_wrong_lt, 'Label type not in this project', 400)
 
-        # Regular insert
-        create_label_helper(request_handler, label, 'Created', 200)
+    # Regular insert
+    create_label_helper(request_handler, label, 'Created', 200)
 
-        # Request with label name which exists in another project
-        label_wrong_lt = {'labelTypeId': 3,
-                          'labelName': 'Angry',
-                          'labelDescription': 'A brilliant idea!',
-                          'p_id': 2}
-        create_label_helper(
-            request_handler, label_wrong_lt, 'Created', 200)
+    # Request with label name which exists in another project
+    label_wrong_lt = {'labelTypeId': 3,
+                      'labelName': 'Angry',
+                      'labelDescription': 'A brilliant idea!',
+                      'p_id': 2}
+    create_label_helper(
+        request_handler, label_wrong_lt, 'Created', 200)
 
-        # Missing tests:
-        # - Frozen project
+    # Missing tests:
+    # - Frozen project
+    # - White space
 
 
 """
@@ -132,10 +131,12 @@ def create_label_helper(request_handler, label, expected_text, expected_status):
     if expected_status == 200:
         # Query on the data provided
         entry = db.session.scalar(select(Label).where(
+            Label.lt_id == label['labelTypeId'],
                 Label.lt_id == label['labelTypeId'], 
-                Label.name == label['labelName'],
-                Label.description ==  label['labelDescription'],
-                Label.p_id == label['p_id']))
+            Label.lt_id == label['labelTypeId'],
+            Label.name == label['labelName'],
+            Label.description == label['labelDescription'],
+            Label.p_id == label['p_id']))
         # There should be a result
         assert entry
 
