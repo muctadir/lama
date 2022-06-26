3eb3b60ebab263a6f22d49ad701804072ba05566
Bartjan Henkemans <b.henkemans@student.tue.nl>
2022-06-26 11:28:30 +0200

    util file tested

---
+++ backend/src/tests/test_util.py
@@ -0,0 +1,11 @@
+from src.conftest import RequestHandler
+
+
+def test_create_name(app, client):
+    """
+        This test checks whether the health check works.
+    """
+    with app.app_context():
+        request_handler = RequestHandler(app, client)
+        response = request_handler.get('health', {}, False)
+        assert response.status_code == 200
