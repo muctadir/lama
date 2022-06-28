4a2ae7ed81a8f124eb7123d531c218de52e4f49f
JarlJansen123 <jarljansen123@gmail.com>
2022-06-28 11:40:47 +0200

    confirm modal test
    confirm modal test

--- frontend/src/app/modals/confirm-modal/confirm-modal.component.spec.ts
+++ frontend/src/app/modals/confirm-modal/confirm-modal.component.spec.ts
@@ -1,14 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 import { ConfirmModalComponent } from './confirm-modal.component';
 
-describe('ConfirmModalComponent', () => {
+fdescribe('ConfirmModalComponent', () => {
   let component: ConfirmModalComponent;
   let fixture: ComponentFixture<ConfirmModalComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ConfirmModalComponent ]
+      declarations: [ ConfirmModalComponent ],
+      providers: [ NgbModal, NgbActiveModal],
+      imports: [RouterTestingModule]
     })
     .compileComponents();
   });
@@ -21,5 +25,18 @@ describe('ConfirmModalComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call the functions in the confirm method', () => {
+    // creates the spies
+    let spy = spyOn(component["confirmEvent"], "emit");
+    let spy2 = spyOn(component["activeModal"], "close");
+
+    // Calls the function which we want to test
+    component.confirm();
+
+    // Checks whether the correct functions are called
+    expect(spy).toHaveBeenCalledWith(true);
+    expect(spy2).toHaveBeenCalled();
   });
 });
