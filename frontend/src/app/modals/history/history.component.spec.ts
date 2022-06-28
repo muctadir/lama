53fe4a23b009220bc30622430be648824be24aa9
JarlJansen123 <jarljansen123@gmail.com>
2022-06-28 10:48:15 +0200

    History test cases
    history test cases

--- frontend/src/app/modals/history/history.component.spec.ts
+++ frontend/src/app/modals/history/history.component.spec.ts
@@ -1,14 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HistoryComponent } from './history.component';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { RouterTestingModule } from '@angular/router/testing';
 
-describe('ThemeHistoryComponent', () => {
+fdescribe('HistoryComponent', () => {
   let component: HistoryComponent;
   let fixture: ComponentFixture<HistoryComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HistoryComponent ]
+      declarations: [ HistoryComponent ],
+      // Adds NgbActiveModal dependency
+      providers: [NgbActiveModal],
+      // Adding the RouterTestingModule dependency
+      imports: [RouterTestingModule]
     })
     .compileComponents();
   });
@@ -21,5 +26,30 @@ describe('ThemeHistoryComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize data correctly', () => {
+    // Creates the spy to check whether the function is called
+    let spy = spyOn(component, "getHistoryData");
+
+    // Calls the function which we will test
+    component.ngOnInit();
+
+    // Does a check
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should initialize data correctly', async () => {
+    // Sets some basic value
+    component.history_type = "Label";
+
+    // Creates the spy to check whether the function is called
+    let spy = spyOn(component["historyService"], "getHistory");
+
+    // Calls the function which we will test
+    component.getHistoryData();
+
+    // Does a check
+    expect(spy).toHaveBeenCalledWith("Label");
   });
 });
