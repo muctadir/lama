62b3dfa749a74bbc55ef407e8f608d1d67273a99
JarlJansen123 <jarljansen123@gmail.com>
2022-06-28 12:51:51 +0200

    toast service test cases
    toast service test cases

--- frontend/src/app/services/app-toast.service.ts
+++ frontend/src/app/services/app-toast.service.ts
@@ -4,16 +4,31 @@ import { Injectable, TemplateRef } from '@angular/core';
   providedIn: 'root'
 })
 export class AppToastService {
+  /* Array containing the toasts */
   toasts: any[] = [];
 
+  /**
+   * Adds a toast to the list of toasts
+   * 
+   * @param textOrTpl the string of the toast
+   * @param options options of the toast
+   */
   show(textOrTpl: string | TemplateRef<any>, options: any = {}) {
     this.toasts.push({ textOrTpl, ...options });
   }
 
+  /**
+   * Removes a toast from the list of toasts
+   * 
+   * @param toast toast to remove from the list
+   */
   remove(toast: any) {
     this.toasts = this.toasts.filter(t => t !== toast);
   }
 
+  /**
+   * Clears the entire list of toasts
+   */
   clear() {
     this.toasts.splice(0, this.toasts.length);
   }
