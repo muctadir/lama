768927a13826e1e80d7329dee001006aa0989444
TheVeerloes <veerlefurst@gmail.com>
2022-06-30 14:45:26 +0200

    added some comments

--- frontend/src/app/modals/toast-global/toast-container.component.ts
+++ frontend/src/app/modals/toast-global/toast-container.component.ts
@@ -1,10 +1,10 @@
+// Imports
 import { Component, TemplateRef } from '@angular/core';
-
 import { AppToastService } from 'app/services/app-toast.service';
-
 
 @Component({
   selector: 'toasts-app',
+  // HTML template for the toast
   template: `
     <ngb-toast
       *ngFor="let toast of toastService.toasts"
@@ -20,11 +20,14 @@ import { AppToastService } from 'app/services/app-toast.service';
       <ng-template #text>{{ toast.textOrTpl }}</ng-template>
     </ngb-toast>
   `,
+  // Where the toast will show up
   host: { 'class': 'toast-container position-fixed top-0 end-0 p-3', 'style': 'z-index: 1200' }
 })
+
 export class ToastsContainer {
   constructor(public toastService: AppToastService) { }
 
+  // Template of the toast
   isTemplate(toast: any) {
     return toast.textOrTpl instanceof TemplateRef;
   }
--- frontend/src/app/services/app-toast.service.ts
+++ frontend/src/app/services/app-toast.service.ts
@@ -1,19 +1,24 @@
+// Imports
 import { Injectable, TemplateRef } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppToastService {
+  // Array of toasts
   toasts: any[] = [];
 
+  // Show the toast
   show(textOrTpl: string | TemplateRef<any>, options: any = {}) {
     this.toasts.push({ textOrTpl, ...options });
   }
 
+  // Remove the toast from the array
   remove(toast: any) {
     this.toasts = this.toasts.filter(t => t !== toast);
   }
 
+  // Clear the toast from the array
   clear() {
     this.toasts.splice(0, this.toasts.length);
   }
