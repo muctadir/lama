d09e13e0f9a1edbb457efe3ce0a36c9169d96a24
Olteniceanu, Ana-Maria <a.olteniceanu@student.tue.nl>
2022-07-03 15:24:41 +0300

    Apply 1 suggestion(s) to 1 file(s)

--- frontend/src/app/project/project-settings/project-settings.component.spec.ts
+++ frontend/src/app/project/project-settings/project-settings.component.spec.ts
@@ -240,7 +240,7 @@ describe('ProjectSettingsComponent', () => {
 
     // Checks the call for the backend
     expect(spy).toHaveBeenCalled();
-    // checks the toast emitted
+    // Checks the toast emitted
     expect(spyToast).toHaveBeenCalledWith([false, "Input contains a forbidden character: \\ ; , or #"]);
     // checks the function call at the end of the error block
     expect(spy2).toHaveBeenCalled();
